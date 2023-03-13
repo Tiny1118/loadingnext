99482dee8b458edc968d17805d915edcf8f7c440
simple123w <126952222+Tiny1118@users.noreply.github.com>
2023-03-13 19:05:16 +0500

    we
    ef

---
+++ my/src/pages/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function index() {
+  const data = [
+    {
+      "title": "Item 1",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a consequat nibh. Mauris suscipit arcu at fermentum convallis. Pellentesque consectetur mi in felis maximus posuere."
+    },
+    {
+      "title": "Item 2",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie tellus a maximus tempus. Duis vel leo iaculis, porttitor erat et, posuere erat. Ut blandit."
+    },
+    {
+      "title": "Item 3",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, nibh imperdiet tempus pharetra, arcu risus aliquet arcu, a auctor ex lacus efficitur purus. Morbi."
+    },
+    {
+      "title": "Item 4",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lobortis nibh, eu molestie est placerat non. Donec ornare nisl erat, non imperdiet elit porta."
+    }
+  ];
+  return (
+    <div>
+
+<div className="App">
+      <div className="content">
+        <div className="app-description">
+          <a href="https://peteraiello.dev" rel="noreferrer" target="_blank"><img src={logo} className="peter-aiello-logo" alt="peter-aiello-logo" /></a>
+          <h1>Custom React Accordion</h1>
+          <p>Choose the section using the Tab key. Press Enter to open. Shift + Tab to go to select the previous section.</p>
+        </div>
+          <AccordionWrapper>
+            {data.map((item, index) => (
+              <AccordionItem key={index} index={index} title={item.title} description={item.description} />
+            ))}
+          </AccordionWrapper>
+      </div>
+      <footer className="footer">
+        <p>Code on <a href="https://github.com/peteraiello/custom-react-accordion" rel="noreferrer" target="_blank">GitHub</a></p>
+      </footer>
+    </div>
+    </div>
+  )
+}
